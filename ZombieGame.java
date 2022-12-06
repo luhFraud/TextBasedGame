4786c3dc3f38c5ef83616022a88eba7f495b549e
luhFraud <spartida0002@gmail.com>
2022-12-05 16:16:49 -0800

    Added the basic layout

--- ZombieGame.java
+++ ZombieGame.java
@@ -1,7 +1,24 @@
 import java.util.*;
 
 public class ZombieGame {
+
+  static String[] health = {"<3","<3", "<3"};
+
   public static void main(String[] args){
+
+    intro();
+
+  }
+  public static void intro() {
+
+    System.out.println("=============================================");
+    System.out.println("\nZombie text based game by: Steven Partida, Evelyn "+
+                        "Martinez, & Yamilena Hernandez");
+    System.out.println("\n=============================================");
+
+  }
+  public static void firstLevel() {
+
 
     
   }
