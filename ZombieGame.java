785852468c37304de5700d23c402cd157109057e
luhFraud <spartida0002@gmail.com>
2022-12-07 19:28:01 -0800

    Added do while loop forfirst question loop.

--- ZombieGame.java
+++ ZombieGame.java
@@ -62,28 +62,33 @@ public class ZombieGame {
                         "We need to find a way to distract them.");
     System.out.println("Josh: Any ideas?");
 
-    System.out.println("\nFind a way to distract the horde. Enter: ");
-    System.out.println(" 1 - Throw a brick at a near by trailer window");
-    System.out.println(" 2 - Place something on top of the horn of Joel's " +
-                        "truck");
     int userChoice;
-    userChoice = input.nextInt();
+    do {
 
-    if(userChoice < 1 || userChoice > 2) {
-      System.out.println("Please enter a valid option");
+      System.out.println("\nFind a way to distract the horde. Enter: ");
+      System.out.println(" 1 - Throw a brick at a near by trailer window");
+      System.out.println(" 2 - Place something on top of the horn of Joel's " +
+                          "truck");
       userChoice = input.nextInt();
-    }
 
-    if (userChoice == 1) {
-      System.out.println("\nThe horde look distracted but only for a " +
-      "momment and went back to banging there way into the trailer.");
-      System.out.println("Josh: That wasn't loud enough. Try something else.");
+      if(userChoice < 1 || userChoice > 2) {
+        System.out.println("Please enter a valid option");
+        userChoice = input.nextInt();
+      }
 
-    } else {
-      System.out.println("\nThe horde seems distracted from the noise " +
-                          "and are walking towards the truck.");
-      System.out.println("Josh: Nice! That should keep them distracted.");
-    }
+      if(userChoice == 1) {
+        System.out.println("\nThe horde look distracted but only for a " +
+        "momment and went back to banging there way into the trailer.");
+        System.out.println("\nJosh: That wasn't loud enough. " +
+                            "Try something else.");
+
+      }
+
+    } while (userChoice != 2);
+
+    System.out.println("\nThe horde seems distracted from the noise " +
+                        "and are walking towards the truck.");
+    System.out.println("\nJosh: Nice! That should keep them distracted.");
 
 
 
