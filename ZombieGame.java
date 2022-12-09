22afe52892d53bac86b7e33ad4d356f3c68b12ae
luhFraud <spartida0002@gmail.com>
2022-12-08 23:12:18 -0800

    Added more to the story

--- ZombieGame.java
+++ ZombieGame.java
@@ -33,6 +33,8 @@ public class ZombieGame {
 
   public static void firstLevel() {
 
+    String selectedDoorItem;
+
     System.out.println("\nMission 1: It is 4:35 pm the sun is starting " +
     "to set. You and the group you were assigned to are heading back to " +
     "the settlement from your daily loot runs in the nearby small town. " +
@@ -86,10 +88,73 @@ public class ZombieGame {
 
     } while (userChoice != 2);
 
+    System.out.println("\nYou pick up a dead corpse and sit him up in the " +
+    "diver seat with his head on the horn.");
     System.out.println("\nThe horde seems distracted from the noise " +
                         "and are walking towards the truck.");
     System.out.println("\nJosh: Nice! That should keep them distracted.");
 
+    enterToCont();
+
+    System.out.println("\nBANG BANG BANG");
+    System.out.println("Luke: Joel you in there! It's Luke!");
+    System.out.println("Joel: Luke!? Alvin let them in!");
+
+
+    System.out.println("\n5:05 pm: You and the group got into the trailer and " +
+                        "found Joel's group.");
+
+    enterToCont();
+
+    System.out.println("\nJoel: Luke what are you doing here?");
+    System.out.println("James: Josh!");
+    System.out.println("Josh: James!");
+    System.out.println("Sarah: Hey christa! How you doing?");
+    System.out.println("Christa: I'm scared Sarah. I thought I was going to" +
+                        " die!");
+    System.out.println("Luke: We were heading back from the loot run when...");
+
+    enterToCont();
+
+    System.out.println("\nLuke's explanation was interrupted by the sudden " +
+                        "silence. The dead corpse you placed in the truck " +
+                        "slid off and now the horde is coming back.");
+
+    enterToCont();
+
+    System.out.println("\nThe horde is now banging at the trailer door.");
+    System.out.println("Alvin: I can't hold them for much longer!" +
+    " Someone find something to hold the door!");
+
+    int doorItem;
+    do {
+
+      System.out.println("\nFind something to hold the door shut: ");
+      System.out.println(" 1 - Wooden Chair");
+      System.out.println(" 2 - Book Shelf");
+      System.out.println(" 3 - Coat Rack");
+      doorItem = input.nextInt();
+
+      if(doorItem < 1 || doorItem > 3){
+        System.out.println("Please enter a valid option");
+        doorItem = input.nextInt();
+      }
+
+      if(doorItem == 1) {
+        System.out.println("\nAlvin: The wooden chair won't work." +
+                            " Find something else.");
+      } else if(doorItem == 3) {
+        System.out.println("\nAlvin: What is the coat rack going to do?" +
+                            " Find something else.");
+      }
+    } while (doorItem != 2);
+    System.out.println("\nAlvin: The book shelf should do.");
+    selectedDoorItem = "book shelf";
+
+    System.out.println("\nSarah: The horde of zombies is all around the " +
+    "trailer. We need to find a way out!");
+    System.out.println("James: We already looked around. Maybe you can find " +
+                        "a way out.");
 
 
   }
